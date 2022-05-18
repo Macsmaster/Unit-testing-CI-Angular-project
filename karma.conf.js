b0310e2ced6951d69f3fcd2cf79420505f832a8b
Miguel Coy <miguelcoytriana@gmail.com>
2022-05-18 00:23:41 -0500

    configuration angular actions, correction type error

--- karma.conf.js
+++ karma.conf.js
@@ -47,7 +47,7 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['ChromeHeadless'],
-    custumLaunchers: {
+    customLaunchers: {
       ChromeHeadlessCI: {
         base: 'ChromeHeadless',
         flags: ['--no-sandbox']
